f67a7b5436e89831a0bfcc1b7fa20571edcd8e31
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-11 14:55:23 -0800

    Convert previous script into a module.

--- 01-rearrange-words/rearrange.py
+++ 01-rearrange-words/rearrange.py
@@ -1,4 +1,4 @@
-# Prints a random Monty Python quote. Will only be run as a script because all lines are always executed.
+# Prints a random Monty Python quote (will be run as an independent module). This file is also dual purpose, serving as both an executable and a module. This uses a conditional to allow the program to run 
 import random
 
 quotes = ("It's just a flesh wound.",
@@ -6,7 +6,11 @@ quotes = ("It's just a flesh wound.",
           "THIS IS AN EX-PARROT!!")
 
 # pick a random number between 0 and 2 (the index of the string in the tuple)
-rand_index = random.randint(0, len(quotes) - 1)
+def random_python_quote():
+  rand_index = random.randint(0, len(quotes) - 1)
+  return quotes[rand_index]
 
-# output the random string
-print quotes[rand_index]
+if __name__ = '__main__':
+  quote = random_python_quote()
+  # output the random string
+  print quote
