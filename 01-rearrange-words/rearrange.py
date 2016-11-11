95c44f73c698fb37a15f0022d27c153f3f657b6c
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-11 15:22:43 -0800

    Generate a random number between 1 & 1 minus the count of the command-line arguments.

--- 01-rearrange-words/rearrange.py
+++ 01-rearrange-words/rearrange.py
@@ -3,5 +3,11 @@
 # module needed to work with command-line arguments.
 import sys
 
-# output system command-line arguments.
-print sys.argv
+# module needed to use random number generators.
+import random
+
+# pick a random number between 1 and last element of the arguments (excludes first argument, filename).
+rand_index = random.randint(1, len(sys.argv) - 1)
+
+# output random index of system command-line arguments between 1 and last element.
+print rand_index
