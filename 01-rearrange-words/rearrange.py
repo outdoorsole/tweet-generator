5703733e960b329a117886833846180f42c2e828
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-11 15:28:58 -0800

    Convert rearrange.py script into a module.

--- 01-rearrange-words/rearrange.py
+++ 01-rearrange-words/rearrange.py
@@ -7,7 +7,12 @@ import sys
 import random
 
 # pick a random number between 1 and last element of the arguments (excludes first argument, filename).
-rand_index = random.randint(1, len(sys.argv) - 1)
+def randomly_generated_index():
+  rand_index = random.randint(1, len(sys.argv) - 1)
+  # output random index of system command-line arguments between 1 and last element.
+  return rand_index
 
-# output random index of system command-line arguments between 1 and last element.
-print rand_index
+if __name__ == '__main__':
+  random_index = randomly_generated_index()
+  # output the randomly generated index.
+  print random_index
