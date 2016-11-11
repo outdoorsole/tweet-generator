420f6d21db4092f5cbaa0fd00a2ac8be48ce2b60
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-11 14:08:49 -0800

    Add comments for random number selector & quote output.

--- 01-rearrange-words/rearrange.py
+++ 01-rearrange-words/rearrange.py
@@ -4,5 +4,8 @@ quotes = ("It's just a flesh wound.",
           "He's not the Messiah. He's a very naughty boy!",
           "THIS IS AN EX-PARROT!!")
 
+# pick a random number between 0 and 2 (the index of the string in the tuple)
 rand_index = random.randint(0, len(quotes) - 1)
+
+# output the random string
 print quotes[rand_index]
