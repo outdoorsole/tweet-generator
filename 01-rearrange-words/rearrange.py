55740bd1e7834dcc34cb37474ead88763cb59653
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-11 15:31:47 -0800

    Refactor: print randomly generated command-line argument from list of args.

--- 01-rearrange-words/rearrange.py
+++ 01-rearrange-words/rearrange.py
@@ -7,12 +7,12 @@ import sys
 import random
 
 # pick a random number between 1 and last element of the arguments (excludes first argument, filename).
-def randomly_generated_index():
+def randomly_generated_sys_word():
   rand_index = random.randint(1, len(sys.argv) - 1)
   # output random index of system command-line arguments between 1 and last element.
-  return rand_index
+  return sys.argv[rand_index]
 
 if __name__ == '__main__':
-  random_index = randomly_generated_index()
+  random_sys_word = randomly_generated_sys_word()
   # output the randomly generated index.
-  print random_index
+  print random_sys_word
