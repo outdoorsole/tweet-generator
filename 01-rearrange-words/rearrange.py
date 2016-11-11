0b65d3498ed8b29e2b999fb5e09c4228593c5fa7
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-11 15:17:42 -0800

    Write script to print command-line arguments.

---
+++ 01-rearrange-words/rearrange.py
@@ -0,0 +1,7 @@
+# Rearrange.py: Randomly rearranges a set of words provided as command-line arguments to the script.
+
+# module needed to work with command-line arguments.
+import sys
+
+# output system command-line arguments.
+print sys.argv
