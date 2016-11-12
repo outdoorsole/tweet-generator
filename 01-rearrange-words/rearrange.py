a63f67c2166ae951c341ba0b34c6f807642b55c3
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-11 16:01:05 -0800

    Refactor to start building random word list (currently stores one word).

--- 01-rearrange-words/rearrange.py
+++ 01-rearrange-words/rearrange.py
@@ -13,17 +13,17 @@ new_word_list = []
 indices = []
 
 # pick a random number between 1 and last element of the arguments (excludes first argument, filename).
-def randomly_generated_sys_word():
+def random_sys_word_list():
   rand_index = random.randint(1, len(sys.argv) - 1)
   if rand_index in indices:
     rand_index = random.randint(1, len(sys.argv) - 1)
   else:
     indices.append(rand_index)
+    new_word_list.append(sys.argv[rand_index])
     print indices
     # output random index of system command-line arguments between 1 and last element.
-    return sys.argv[rand_index]
+  return new_word_list
 
 if __name__ == '__main__':
-  random_sys_word = randomly_generated_sys_word()
-  new_word_list = random_sys_word
-  print new_word_list
+  random_word_list = random_sys_word_list()
+  print random_word_list
