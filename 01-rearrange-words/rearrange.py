7c7d99da1c6638169742ed79c146e9775c7e1d63
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-12 11:12:57 -0800

    Rename variables for consistency (word_list).

--- 01-rearrange-words/rearrange.py
+++ 01-rearrange-words/rearrange.py
@@ -9,23 +9,25 @@ import random
 # create a new list, which excludes first argument, filename.
 def sys_word_list():
   # variable to store new shuffled list.
-  new_word_list = []
+  word_list = []
   
   for word in sys.argv:
-    new_word_list.append(word)
-  new_word_list.pop(0)
-  return new_word_list
+    word_list.append(word)
+
+  # remove the first element (filename).
+  word_list.pop(0)
+  return word_list
 
 # pick a random number between 1 and last element of the arguments (excludes first argument, filename).
-def random_index_generator(list):
-  rand_index = random.randint(1, len(list) - 1)
+def random_index_generator(word_list):
+  rand_index = random.randint(1, len(word_list) - 1)
   return rand_index
 
-def shuffle_words(list):
+def shuffle_words(word_list):
   # variable to store randomly selected indices.
   index = []
 
-  index = random_index_generator(list)
+  index = random_index_generator(word_list)
   print index
 
   word_list[0], word_list[index] = word_list[index], word_list[0]
@@ -53,8 +55,8 @@ def shuffle_words(list):
 
 
 if __name__ == '__main__':
-  word_list = sys_word_list()
-  print word_list
+  sys_arg_word_list = sys_word_list()
+  print sys_arg_word_list
 
-  shuffled_list = shuffle_words(word_list)
+  shuffled_list = shuffle_words(sys_arg_word_list)
   print shuffled_list
