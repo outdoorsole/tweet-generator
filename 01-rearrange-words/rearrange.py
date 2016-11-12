5a204a8289a7b3e4b4762e82719f7c3d71258e54
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-12 10:07:33 -0800

    Add comments for sys_word_list function.

--- 01-rearrange-words/rearrange.py
+++ 01-rearrange-words/rearrange.py
@@ -12,18 +12,18 @@ new_word_list = []
 # variable to store randomly selected indices.
 indices = []
 
-# pick a random number between 1 and last element of the arguments (excludes first argument, filename).
-def random_sys_word_list():
+# create a new list, which excludes first argument, filename.
+def sys_word_list():
   for word in sys.argv:
     new_word_list.append(word)
   new_word_list.pop(0)
   return new_word_list
 
-# pick a random number
+# pick a random number between 1 and last element of the arguments (excludes first argument, filename).
 def random_index_generator():
   rand_index = random.randint(1, len(sys.argv) - 1)
   return rand_index
 
 if __name__ == '__main__':
-  random_word_list = random_sys_word_list()
-  print random_word_list
+  word_list = sys_word_list()
+  print word_list
