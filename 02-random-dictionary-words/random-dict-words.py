64166b05c5caee2148f72c511e37f62c7c1f8139
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-14 14:08:52 -0800

    Convert program into module & encapsulate function: - create_word_list function: reads a file and returns a generated word list.

--- 02-random-dictionary-words/random-dict-words.py
+++ 02-random-dictionary-words/random-dict-words.py
@@ -1,11 +1,17 @@
 # Random-dict-words: Read from a text file, select a random set of words from the file, and put those words together into a "sentence".
 
-file = open("words")
+# Reads a file and generates a word list.
+def create_word_list():
+  file = open("words")
 
-# variable to store the dictionary words.
-word_list = []
+  # variable to store the dictionary words.
+  word_list = []
 
-for word in file:
-  word_list.append(word)
+  for word in file:
+    word_list.append(word)
 
-print(word_list)
+  return(word_list)
+
+if __name__ == '__main__':
+  dictionary_list = create_word_list()
+  print(dictionary_list)
