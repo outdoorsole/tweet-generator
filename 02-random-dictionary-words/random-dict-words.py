a3e86afc9dd0fe730d192ebcbc8eae368a3fa4d5
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-14 14:04:19 -0800

    Open file & place word contents into words_list.

--- 02-random-dictionary-words/random-dict-words.py
+++ 02-random-dictionary-words/random-dict-words.py
@@ -1,2 +1,11 @@
 # Random-dict-words: Read from a text file, select a random set of words from the file, and put those words together into a "sentence".
 
+file = open("words")
+
+# variable to store the dictionary words.
+word_list = []
+
+for word in file:
+  word_list.append(word)
+
+print(word_list)
