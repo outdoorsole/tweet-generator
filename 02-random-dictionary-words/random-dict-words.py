6c3452fde45867c08f627741a445a27abe8d1bee
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-15 00:02:57 -0800

    Add convert_list_to_string function: - converts the list into a string (sentence) - read sys.argv to get the number of words to display.

--- 02-random-dictionary-words/random-dict-words.py
+++ 02-random-dictionary-words/random-dict-words.py
@@ -1,6 +1,7 @@
 # Random-dict-words: Read from a text file, select a random set of words from the file, and put those words together into a "sentence".
 
 import random
+import sys
 
 # Reads a file and generates a word list.
 def create_word_list():
@@ -10,12 +11,13 @@ def create_word_list():
   word_list = []
 
   for word in file:
+    word = word.strip('\n')
     word_list.append(word)
 
   return(word_list)
 
 def get_word_count(word_list):
-  return len(word_list)
+  return int(sys.argv[1])
 
 def get_random_number_list(word_list, word_count):
     return random.sample(range(len(word_list)), word_count)
@@ -31,12 +33,13 @@ def randomize_word_list(word_list, word_count):
 
     return new_word_list
 
+def convert_list_to_string(word_list):
+    return " ".join(word_list)
+
+
 if __name__ == '__main__':
   dictionary_list = create_word_list()
   word_count = get_word_count(dictionary_list)
   new_dictionary_list = randomize_word_list(dictionary_list, word_count)
-
-  # print dictionary_list
-  print new_dictionary_list
-  print '-------'
-  print 'Word count: ', word_count
+  dictionary_string = convert_list_to_string(new_dictionary_list)
+  print dictionary_string
