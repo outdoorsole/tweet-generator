3bf35e3ab70e795b34bafd362f4cccf6ee552675
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-16 14:08:17 -0800

    Create a list of tuples for word type probabilities.

--- 04-stochastic-sampling/sampling.py
+++ 04-stochastic-sampling/sampling.py
@@ -15,18 +15,35 @@ def generate_word_list():
 def histogram(word_list):
   # returns a histogram data structure that stores each unique word along with the number of times the word appears in the source text.
   print(word_list)
+  word_types = 0
+  word_tokens = 0
   array_of_word_tuples = []
   for index, word in enumerate(word_list):
     word_occurence = word_list.count(word_list[index])
     word_tuple = (word, word_occurence)
     if word_tuple not in array_of_word_tuples:
       array_of_word_tuples.append(word_tuple)
-  return array_of_word_tuples
+  for word, token in array_of_word_tuples:
+    print token
+    word_tokens += token
+    print "this: ", word_tokens
+  return array_of_word_tuples, word_types, word_tokens
+
+def probability(list_of_tuples, word_tokens):
+  word_list_probability = []
+  for word, token in list_of_tuples:
+    print word, token, word_tokens
+    word_probability = (float(token) / word_tokens)
+    print "word_probability ", word_probability
+    word_list_probability.append((word, word_probability))
+  return word_list_probability
 
 def random_word(word_list):
   print 'random'
 
 if __name__ == '__main__':
   words = generate_word_list()
-  word_histogram = histogram(words)
-  print word_histogram
+  word_histogram, total_words, unique_words = histogram(words)
+  print word_histogram, total_words, unique_words
+  word_probability_array = probability(word_histogram, unique_words)
+  print word_probability_array
