2e4031c71b200832c8a7543af539223f320dc3c6
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-16 23:05:42 -0800

    Implement new generate_words_list() to return word list, add comments.

--- 03-analyze-word-frequency/analyze2.py
+++ 03-analyze-word-frequency/analyze2.py
@@ -9,8 +9,13 @@
 # A frequency() function that takes a word and histogram argument and returns the number of times that word appears in a text. For example, when given the word "mystery" and the Holmes histogram, it will return the integer 20.
 
 def generate_word_list():
+  # Create file object using the open() function to process the text file.
   file = open('the-three-fates.txt')
-  return file
+  # Read entire file into a single string.
+  text = file.read()
+  # Return a list of words from the string.
+  words_list = text.split()
+  return file, words_list
 
 def histogram(word_list):
   return None
@@ -22,5 +27,5 @@ def frequency(word, histogram):
   return None
 
 if __name__ == '__main__':
-  words = generate_word_list()
+  all_words, words = generate_word_list()
   print words
