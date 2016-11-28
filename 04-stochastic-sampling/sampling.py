83c1f4feae99def89d2fcfec2cdb37e0972948d5
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-11-28 05:36:04 -0800

    Fix probability function to generate correct percentages: - Create tokens method to calculate the list of word tokens. - Change inaccurate naming (correctly define types and tokens).

--- 04-stochastic-sampling/sampling.py
+++ 04-stochastic-sampling/sampling.py
@@ -3,12 +3,16 @@
 def generate_word_list():
   # Create file object using the open() function to process the text file.
   # Specify the context using the with statement. A file object is a context manager; the with statement uses the file as a manager. OS resources are released when Python files are closed.
-  with open('the-three-fates.txt') as file:
+  with open('words.txt') as file:
     # Read entire file into a single string.
     text = file.read()
     # Return a list of words from the string.
     words_list = text.split()
   return words_list
+
+def sort_word_list(word_list):
+  # return sorted(word_list, key = lambda x: x[0])
+  return sorted(word_list)
 
 def histogram(word_list):
   # returns a histogram data structure that stores each unique word along with the number of times the word appears in the source text.
@@ -30,26 +34,32 @@ def histogram(word_list):
   if count > 1 or word_list[-1] != word_list[-2]:
     word_tuple = (word_list[-1], count)
     word_tuples_list.append(word_tuple)
-  print count
-  print counter
   return word_tuples_list
 
-
-def probability(list_of_tuples, word_tokens):
+# Types: total unique words
+# Tokens: total concrete particulars
+def probability(list_of_tuples, tokens):
   word_list_probability = []
-  for word, token in list_of_tuples:
-    print word, token, word_tokens
-    word_probability = (float(token) / word_tokens)
+  for word, count in list_of_tuples:
+    word_probability = (float(count) / tokens)
     word_list_probability.append((word, word_probability))
   return word_list_probability
+
+def tokens(list_of_tuples):
+  tokens = 0
+  for word, count in list_of_tuples:
+    tokens += count
+  return tokens
 
 def random_word(word_list):
   print 'random'
 
 if __name__ == '__main__':
   words = generate_word_list()
-  print words
-  # word_histogram, total_words, unique_words = histogram(words)
-  # print word_histogram, total_words, unique_words
-  # word_probability_array = probability(word_histogram, unique_words)
-  # print word_probability_array
+  sorted_word_list = sort_word_list(words)
+  print 'sorted_word_list:', sorted_word_list
+  words_histogram = histogram(sorted_word_list)
+  print 'words_histogram:', words_histogram
+  word_tokens = tokens(words_histogram)
+  word_probability_list = probability(words_histogram, word_tokens)
+  print 'word_probability_list:', word_probability_list
