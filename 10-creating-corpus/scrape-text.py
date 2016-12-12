1477c5c1d2eb154e2a52b33352206b51139cfcdc
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-12-12 11:00:41 -0800

    Remove new line from each sentence.

--- 10-creating-corpus/scrape-text.py
+++ 10-creating-corpus/scrape-text.py
@@ -1,5 +1,15 @@
 def output_file():
-  bookfile = open('nature.txt').read()
-  print bookfile
+  # open file
+  file = open('nature.txt')
+  # read individual line
+  sentence_list = file.readlines()
+  print sentence_list
+
+  new_sentence_list = []
+  for sentence in sentence_list:
+    new_sentence = sentence.strip('\n')
+    print new_sentence
+    new_sentence_list.append(new_sentence)
+  print new_sentence_list
 
 output_file()
