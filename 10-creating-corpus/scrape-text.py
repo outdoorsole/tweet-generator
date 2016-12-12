8297753ecbae8a71456ecabea285eb1aba409e36
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-12-12 11:23:40 -0800

    Create test file to output data: - test.txt will store the output of the scrape-text script. - temporarily: output all text from book as a string.

--- 10-creating-corpus/scrape-text.py
+++ 10-creating-corpus/scrape-text.py
@@ -2,14 +2,18 @@ def output_file():
   # open file
   file = open('nature.txt')
   # read individual line
-  sentence_list = file.readlines()
-  print sentence_list
+  book_string = file.read()
+  file.close()
 
-  new_sentence_list = []
-  for sentence in sentence_list:
-    new_sentence = sentence.strip('\n')
-    print new_sentence
-    new_sentence_list.append(new_sentence)
-  print new_sentence_list
+  print book_string
+  new_file = open('test.txt', 'w')
+  new_file.write(book_string)
+
+  # new_sentence_list = []
+  # for sentence in sentence_list:
+  #   new_sentence = sentence.strip('\n')
+  #   print new_sentence
+  #   new_sentence_list.append(new_sentence)
+  # print new_sentence_list
 
 output_file()
