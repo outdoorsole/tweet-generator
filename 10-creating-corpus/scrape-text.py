091648cdd5f244a3e6224082b9eb1bb6a356a564
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-12-12 22:21:31 -0800

    Isolate contents of book (parse out license agreement).

--- 10-creating-corpus/scrape-text.py
+++ 10-creating-corpus/scrape-text.py
@@ -11,6 +11,16 @@ def output_file():
   book_array = book_string.split('INTRODUCTION.')
 
   # This is the content of the book.
-  print book_array[1]
+  book_string_wo_intro = book_array[1]
+
+  # Split the Licensing content from the content of the book.
+  book_array = book_string_wo_intro.split('End of the Project Gutenberg EBook of Nature,')
+  
+  # Contents of book
+  book_content = book_array[0]
+
+  # Remove whitespace at the end of the content.
+  print book_content.rstrip()
+
 
 output_file()
