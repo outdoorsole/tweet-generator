0c7f1fe5b4d1198f625316d050c996bd18cf3d75
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-12-12 22:10:36 -0800

    Remove the introduction from the content.

--- 10-creating-corpus/scrape-text.py
+++ 10-creating-corpus/scrape-text.py
@@ -7,4 +7,10 @@ def output_file():
   book_string = file.read()
   file.close()
   
+  # Split the Introduction apart from the content of the book.
+  book_array = book_string.split('INTRODUCTION.')
+
+  # This is the content of the book.
+  print book_array[1]
+
 output_file()
