ea854fc7a837cc1c94f5b243911b4645236dc951
Maribel Montejano <outdoorsole@users.noreply.github.com>
2016-12-12 22:01:49 -0800

    Redo scrape-text file: - Begin parsing by creating a string of book content.

--- 10-creating-corpus/scrape-text.py
+++ 10-creating-corpus/scrape-text.py
@@ -7,28 +7,4 @@ def output_file():
   book_string = file.read()
   file.close()
   
-  word_string = re.sub('(\W+)', " ", book_string, )
-  pattern = re.compile('(\w+?!.)', book_string)
-  print pattern.group(1)
-
-  # word_list = word_string.split(" ")
-  # print word_list
-
-
-  # new_file = open('test.txt', 'a')
-  # new_word_list = []
-  # for word in word_list:
-  #   new_file.write(word)
-  #   new_word_list.append(word)
-
-  # print new_word_list
-
-
-  # new_sentence_list = []
-  # for sentence in sentence_list:
-  #   new_sentence = sentence.strip('\n')
-  #   print new_sentence
-  #   new_sentence_list.append(new_sentence)
-  # print new_sentence_list
-
 output_file()
